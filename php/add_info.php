6d6401b0961385d91a79d44c10cdbd3631ddc8db
mewCu <710010358@qq.com>
2021-01-15 00:22:28 +0800

    #4

--- php/add_info.php
+++ php/add_info.php
@@ -1,7 +1,7 @@
 <?php
-    $email = $_GET["email"];
-    $username = $_GET["username"];
-    $flag = $_GET["flag"];
+    $email = $_POST["email"];
+    $username = $_POST["username"];
+    $flag = $_POST["flag"];
     $fp = fopen(include $_SERVER['DOCUMENT_ROOT']."/php/dst_info_event",'a');
     fwrite($fp,PHP_EOL.$email.":".$username.":".$flag);
     fclose($fp);
