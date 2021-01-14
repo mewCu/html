a988c4b46a4f9dadfde03e256afca530af6e1df7
mewCu <710010358@qq.com>
2021-01-15 00:09:27 +0800

    #2

--- php/add_info.php
+++ php/add_info.php
@@ -2,7 +2,8 @@
     $email = $_GET["email"];
     $username = $_GET["username"];
     $flag = $_GET["flag"];
-    $fp = fopen("./dst_info_event",'a');
+    $fp = fopen("./php/dst_info_event",'a');
     fwrite($fp,PHP_EOL.$email.":".$username.":".$flag);
     fclose($fp);
+    echo("success")
 ?>
